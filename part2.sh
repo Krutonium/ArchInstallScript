79b0349997519e065e5baec29be4c0dfbb375c70
Krutonium <PFCKrutonium@gmail.com>
2018-08-25 05:06:57 -0400

    More Packages, Fixes

--- part2.sh
+++ part2.sh
@@ -2,7 +2,7 @@
 rm -rf /etc/localtime
 ln -sf /usr/share/zoneinfo/America/Toronto /etc/localtime
 hwclock --systohc
-sed -i '/en_US.UTF8/s/^#//g' /etc/locale.gen
+echo en_US.UTF-8 >> /etc/locale.gen
 locale-gen
 echo LANG=en_US.UTF-8 >> /etc/locale.conf
 echo KruArch >> /etc/hostname
@@ -13,11 +13,16 @@ echo USER PASSWORD:
 useradd -m -g wheel -s /bin/bash krutonium
 passwd krutonium
 echo 'krutonium	ALL=(ALL:ALL) ALL' >> /etc/sudoers
-pacman -Syu intel-ucode grub os-prober
+
+pacman -S archlinux-keyring --noconfirm
+pacman-key --init
+pacman-key --populate archlinux
+
+pacman -Syu intel-ucode grub os-prober --noconfirm
 grub-install --target=i386-pc /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
 
-pacman -S networkmanager gnome budgie-desktop
+pacman -S networkmanager gnome budgie-desktop archlinux-wallpaper network-manager-applet --noconfirm
 systemctl enable gdm
 systemctl enable NetworkManager
 
