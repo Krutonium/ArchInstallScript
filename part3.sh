4cf2d6b3f6bea3a45057a301ee945564d69167db
Krutonium <3945538+Krutonium@users.noreply.github.com>
2018-11-16 19:25:22 -0500

    Update part3.sh

--- part3.sh
+++ part3.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
+BUILDDIR=~/BUILD
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
-
+rm -rd ~/BUILD
 #now we have yay installed.
 
 yay -S pamac-aur discord budgie-network-applet openrct2 telegram-desktop --noconfirm
